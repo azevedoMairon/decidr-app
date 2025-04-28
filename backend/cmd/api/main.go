61f94ef07db8046827bc8a9e282523483408e312
azevedoMairon <maironazevedo@gmail.com>
2025-04-28 18:18:26 -0300

    feat: Add get participants route

--- backend/cmd/api/main.go
+++ backend/cmd/api/main.go
@@ -6,6 +6,7 @@ import (
 
 	"github.com/azevedoMairon/decidr-app/infra/mongo"
 	"github.com/azevedoMairon/decidr-app/infra/mongo/migrations"
+	"github.com/azevedoMairon/decidr-app/internal/participants"
 	"github.com/gin-gonic/gin"
 )
 
@@ -21,7 +22,17 @@ func main() {
 		log.Fatal(err)
 	}
 
+	repo := participants.NewRepository(db)
+	service := participants.NewService(repo)
+	handler := participants.NewHandler(service)
+
 	router := gin.Default()
+
+	router.GET("/", func(c *gin.Context) {
+		c.String(200, "OK!")
+	})
+
+	router.GET("/api/participants", handler.GetParticipants)
 
 	router.Run(":8080")
 }
---
+++ backend/internal/participants/endpoint.go
@@ -0,0 +1,27 @@
+package participants
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+type Handler struct {
+	service Service
+}
+
+func NewHandler(s Service) *Handler {
+	return &Handler{service: s}
+}
+
+func (h *Handler) GetParticipants(c *gin.Context) {
+	ctx := c.Request.Context()
+
+	participants, err := h.service.GetParticipants(ctx)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+
+	c.JSON(http.StatusOK, participants)
+}
