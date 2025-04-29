8383e82e33bbbbe0add0b191488e36caa1661a74
azevedoMairon <maironazevedo@gmail.com>
2025-04-29 20:55:15 -0300

    feat: Create react-app using vitejs

---
+++ frontend/src/App.tsx
@@ -0,0 +1,19 @@
+function App() {
+
+  return (
+    <>
+      <h1 className="font-display text-6xl font-bold text-white">
+        DECIDR.
+      </h1>
+
+      <p className="text-dusk mt-4 font-sans">
+        Escolha seu favorito e veja os resultados em tempo real.
+      </p>
+      <button className="mt-6 px-4 py-2 bg-highlight text-midnight font-bold rounded">
+        Votar Agora
+      </button>
+    </>
+  )
+}
+
+export default App
