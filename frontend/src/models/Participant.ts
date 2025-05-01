c24d57692ba1cc6fbb63546b1471681e1e729761
azevedoMairon <maironazevedo@gmail.com>
2025-05-01 16:24:13 -0300

    feat: Add id component in participant model

--- frontend/src/models/Participant.ts
+++ frontend/src/models/Participant.ts
@@ -1,10 +1,12 @@
 export default class Participant {
+  id: string;
   name: string;
   imageUrl: string;
   isEliminated: boolean;
   isNominated: boolean;
 
   constructor(data: any) {
+    this.id = data.id;
     this.name = data.name;
     this.imageUrl = data.image_url;
     this.isEliminated = data.is_eliminated;
