599f9ce3b5f05025dd1e809a18333f037a0e50e3
azevedoMairon <maironazevedo@gmail.com>
2025-05-05 15:58:32 -0300

    chore: Update dockerfile

--- frontend/entrypoint.sh
+++
@@ -1,8 +0,0 @@
-if [ -z "$BACKEND_URL" ]; then
-  echo "Erro: BACKEND_URL não está definido."
-  exit 1
-fi
-
-envsubst '$BACKEND_URL' < /etc/nginx/templates/nginx.conf.template > /etc/nginx/conf.d/default.conf
-
-exec nginx -g 'daemon off;'
